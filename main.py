fb27bdcc5f45d3a51704e0f70ae025bc92f21c96
sebwelsch <sebjak1@hotmail.com>
2021-01-22 14:18:46 +0100

    commit

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ kivy.require('1.11.1')
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 
 class frontend(GridLayout):
 
     def __init__(self, **kwargs):
-            self.cols = 2
+        super(frontend, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text = 'SNE'))
+        self.add_widget(Label(text = 'SNE'))
 
 class vejrVejledning(App):
 
