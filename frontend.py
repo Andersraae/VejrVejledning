39616f44c845f705083b2ce011de65f12d0fd9c9
sebwelsch <sebjak1@hotmail.com>
2021-01-22 09:18:45 +0100

    Kivy App

---
+++ frontend.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require('1.11.1')
+
+from kivy.app import App
+
+class
+
+class vejrVejledning(App):
+
+    def build(self):
+        return
+
+
+if __name__ == '__main__':
+    MyApp().run()
