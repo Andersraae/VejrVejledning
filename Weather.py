aabacc6828f1e174f1b4b271816ffb8b9f96383c
Andersraae <andersraae01@gmail.com>
2021-02-05 08:38:02 +0100

    Weather done

--- Weather.py
+++ Weather.py
@@ -3,15 +3,16 @@ import Import as Import
 def sortInfo():
 
     location = Import.getLocation()
-    print(location)
     city = location["countryCapital"]
     returnLocation = [round(location["longitude"],2), round(location["latitude"],2)]
     accuracyRadius = location["accuracyRadius"]
 
     weather = Import.getWeather(city, returnLocation[0], returnLocation[1])
     print(weather)
+    temp = weather["main"]["temp"] - 273,15
+    visibility = weather["visibility"]
+    wind = weather["wind"]["speed"]
 
-    return returnLocation
+    dict = {"city": city, "returnLocation": returnLocation, "accuracyRadius": accuracyRadius, "temp": temp, "visibility": visibility, "wind": wind}
 
-returnLocation = sortInfo()
-print(returnLocation[0],returnLocation[1])
+    return dict
