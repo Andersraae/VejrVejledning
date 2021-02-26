aaa161c3f102c928631cb0cebf9f01e826c020c1
Andersraae <andersraae01@gmail.com>
2021-02-26 08:27:33 +0100

    Error

--- Weather.py
+++ Weather.py
@@ -8,7 +8,6 @@ def sortInfo():
     accuracyRadius = location["accuracyRadius"]
 
     weather = Import.getWeather(city, returnLocation[0], returnLocation[1])
-    print(weather)
     temp = weather["main"]["temp"] - 273,15
     visibility = weather["visibility"]
     wind = weather["wind"]["speed"]
@@ -16,3 +15,34 @@ def sortInfo():
     dict = {"city": city, "returnLocation": returnLocation, "accuracyRadius": accuracyRadius, "temp": temp, "visibility": visibility, "wind": wind}
 
     return dict
+
+def dangerRating():
+
+    dict = sortInfo()
+    if dict["wind"] > 3:
+        windDanger = 1
+    elif dict["wind"] > 8:
+        windDanger = 2
+    elif dict["wind"] > 15:
+        windDanger = 3
+    elif dict["wind"] > 20:
+        windDanger = 4
+    elif dict["wind"] < 20:
+        windDanger = 5
+    else:
+        print("ERROR: windDanger")
+
+    if dict["visibility"] > 500:
+        visibilityDanger = 5
+    elif dict["visibility"] > 2000:
+        visibilityDanger = 4
+    elif dict["visibility"] > 5000:
+        visibilityDanger = 3
+    elif dict["visibility"] > 10000:
+        visibilityDanger = 2
+    elif dict["visibility"] < 10000:
+        visibilityDanger = 1
+    else:
+        print("ERROR: visibilityDanger")
+
+    return windDanger, visibilityDanger
