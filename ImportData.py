dde90983ffb1755bd1f2b3f3d5298ff0e4a114dc
Andersraae <andersraae01@gmail.com>
2022-10-13 14:09:12 +0200

    Udløbet nøgle

--- ImportData.py
+++ ImportData.py
@@ -5,7 +5,7 @@ url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 querystring = {"q":"London","days":"3"}
 
 headers = {
-    'x-rapidapi-key': "a6bf1992eamsh73f99bb10e52ff4p1f181djsnb1ae54ef1d78",
+    'x-rapidapi-key': "",
     'x-rapidapi-host': "weatherapi-com.p.rapidapi.com"
     }
 
